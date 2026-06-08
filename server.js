0e62fbdffa0901267761941ebee4590a1164a5c2
Prachi0629 <prachi.joshi0629@gmail.com>
2026-06-08 09:31:16 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const cors = require("cors");
-
+const pool = require('./db');
 const app = express();
 
 app.use(cors({
@@ -10,6 +10,17 @@ app.use(cors({
 app.use(express.json());
 
 console.log("SERVER STARTED");
+
+app.get('/test-db', async (req, res) => {
+  try {
+    const result = await pool.query('SELECT NOW()');
+    res.json(result.rows[0]);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json(err.message);
+  }
+});
+
 
 try {
   const authRoutes = require("./routes/auth");
